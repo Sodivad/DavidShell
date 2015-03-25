d9c647ca0d64addff4167fa74ad86bd2382cb3ef
David Redondo <4452hex@gmail.com>
2015-03-25 12:57:45 +0100

    Inital commit

---
+++ davidshell.c
@@ -0,0 +1,58 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <pwd.h>
+#include <string.h>
+
+void  shellprint();
+char* readline();
+
+
+int main(int argc, char** argv){
+  
+  while(1){
+    shellprint();
+    char* command = readline();
+    printf("%s\n",command);
+  }
+}
+
+void shellprint(){
+  struct passwd* pw = getpwuid(geteuid());
+  char* wd = getcwd(NULL,0);
+  char host[255];
+  gethostname(host,255);
+  
+  if(!strncmp(wd,pw->pw_dir,strlen(pw->pw_dir))){
+    wd += strlen(pw->pw_dir)-1;
+    *wd = '~';
+  }
+  printf("%s:%s:%s$ ",host,pw->pw_name,wd);
+}
+
+char* readline(){
+  size_t length = 80;
+  char* line = malloc(length);
+  char c;
+  int len = 0; 
+  if(!line){
+    printf("malloc fail");
+    exit(1);
+  }
+ 
+  while(1){
+    c = getchar();
+    if(c=='\n'||c==EOF){
+      line[len] = '\0';
+      return line;
+    }
+    line[len] = c;
+    ++len;
+    if(len%length==0){
+      line = realloc(line,len+length);
+      if(line==NULL){
+	printf("realloc fail");
+      }
+    }
+  }
+}
